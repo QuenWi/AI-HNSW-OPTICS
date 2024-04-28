bdf1b0479f24245c64058d9adf9ec068c10f3852
QuenWi <116632832+QuenWi@users.noreply.github.com>
2024-04-28 11:46:51 +0200

    Update HNSW_Example_Run.py

--- HNSW_Example_Run.py
+++ HNSW_Example_Run.py
@@ -1,6 +1,6 @@
 import HNSW_OPTICS
 from subscripts import datasets
 
-dataset = datasets.dataset_6()
+dataset = datasets.dataset_2()
 datasets.showPlot(dataset)
 HNSW_OPTICS.hnsw_optics(dataset, 5, 28)
